90ba7b6b8dfb06f7853ce4c0bc9fcd9563388b83
yamy <katsushi.yamanaka@w.ntt.com>
2019-11-30 23:43:31 +0900

    test: add category

--- js/content.js
+++ js/content.js
@@ -16,6 +16,7 @@ $(function(){
     channel_elements_div = target.getElementsByClassName('p-channel_sidebar__static_list')[0]
     add_prefix_to_channel(channel_elements_div)
     change_channel_order(channel_elements_div)
+    add_category(channel_elements_div)
   })
   monitor.observe(target, {childList: true})
 })
@@ -44,7 +45,6 @@ function change_channel_order(channel_elements_div) {
       return
     }
     channel_name = channel_name_spans[0].textContent
-    logger.debug(channel_name)
     if (channel_name == parent_channel_name) {
       parent_channel = channel_div
     } else if (channel_name.match(child_channel_name_regex)) {
@@ -56,4 +56,47 @@ function change_channel_order(channel_elements_div) {
     channel_elements_div.insertBefore(child_channel, parent_channel.nextSibling)
     parent_channel = child_channel
   })
+}
+
+// category を追加する
+function add_category(channel_elements_div) {
+  category_name = 'lab3ec'
+  parent_channel_name = 'bear-jp-all-ope'
+
+  parent_channel = null
+  Array.from(channel_elements_div.children).forEach(channel_div => {
+    channel_name_spans = channel_div.getElementsByClassName('p-channel_sidebar__name')
+    if (channel_name_spans.length != 1) {
+      return
+    }
+    channel_name = channel_name_spans[0].textContent
+    if (channel_name == parent_channel_name) {
+      parent_channel = channel_div
+    }
+  })
+
+  // google material icon 使うために header に link 仕込む
+  icon_font_link = document.createElement('link')
+  icon_font_link.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons')
+  icon_font_link.setAttribute('rel', 'stylesheet')
+  document.head.appendChild(icon_font_link)
+
+  category_name_span = document.createElement('span')
+  category_name_span.textContent = category_name + ' /'
+
+  category_icon_i = document.createElement('i')
+  category_icon_i.classList.add('material-icons')
+  category_icon_i.textContent = 'category'
+
+  category_div = document.createElement('div')
+  // これつけたらいい感じの位置に表示してくれる
+  category_div.classList.add('p-channel_sidebar__link')
+  category_div.appendChild(category_icon_i)
+  category_div.appendChild(category_name_span)
+
+  listitem_div = document.createElement('div')
+  listitem_div.setAttribute('role', 'listitem')
+  listitem_div.appendChild(category_div)
+
+  channel_elements_div.insertBefore(listitem_div, parent_channel.nextSibling)
 }
