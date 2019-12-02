94df8017bd0604a5da125c3cfab265cbf80fa4b3
yamy <katsushi.yamanaka@w.ntt.com>
2019-12-02 11:27:41 +0900

    test: category section, channel in category

--- js/content.js
+++ js/content.js
@@ -11,12 +11,14 @@ $(function(){
 
   var target = document.getElementsByClassName('p-client_container')[0]
   var monitor = new MutationObserver(function(){
-    logger.debug('hi!')
     // channel の div たちを保持している div
     channel_elements_div = target.getElementsByClassName('p-channel_sidebar__static_list')[0]
     add_prefix_to_channel(channel_elements_div)
     change_channel_order(channel_elements_div)
+    add_category_section(channel_elements_div)
+    register_storage()
     add_category(channel_elements_div)
+    add_channel_to_category(channel_elements_div)
   })
   monitor.observe(target, {childList: true})
 })
@@ -58,22 +60,47 @@ function change_channel_order(channel_elements_div) {
   })
 }
 
+// category section を追加する
+function add_category_section(channel_elements_div) {
+  starred = null
+  Array.from(channel_elements_div.children).forEach(div => {
+    section_name_spans = div.getElementsByClassName('p-channel_sidebar__section_heading_label')
+    if (section_name_spans.length != 1) {
+      return
+    }
+    section_name = section_name_spans[0].textContent
+    logger.debug(section_name)
+    if (section_name == ' Starred') {
+      starred = div
+      logger.debug('find starred')
+    }
+  })
+
+  logger.debug(starred)
+
+  section_heading = document.createElement('div')
+  section_heading.classList.add('p-channel_sidebar__section_heading')
+  // 他と合わせていい感じに太字にしてくれる
+  section_heading.classList.add('p-channel_sidebar__section_heading_label')
+  section_heading.textContent = 'Categories'
+
+  categories = document.createElement('div')
+  categories.setAttribute('role', 'listitem')
+  categories.classList.add('categories')
+  categories.appendChild(section_heading)
+
+  channel_elements_div.insertBefore(categories, starred) 
+  
+  presentation = document.createElement('div')
+  presentation.setAttribute('role', 'presentation')
+  presentation.style.height = '10px'
+
+  channel_elements_div.insertBefore(presentation, starred)
+}
+
 // category を追加する
 function add_category(channel_elements_div) {
   category_name = 'lab3ec'
-  parent_channel_name = 'bear-jp-all-ope'
-
-  parent_channel = null
-  Array.from(channel_elements_div.children).forEach(channel_div => {
-    channel_name_spans = channel_div.getElementsByClassName('p-channel_sidebar__name')
-    if (channel_name_spans.length != 1) {
-      return
-    }
-    channel_name = channel_name_spans[0].textContent
-    if (channel_name == parent_channel_name) {
-      parent_channel = channel_div
-    }
-  })
 
   // google material icon 使うために header に link 仕込む
   icon_font_link = document.createElement('link')
@@ -82,15 +109,17 @@ function add_category(channel_elements_div) {
   document.head.appendChild(icon_font_link)
 
   category_name_span = document.createElement('span')
-  category_name_span.textContent = category_name + ' /'
+  category_name_span.textContent = category_name
+  category_name_span.classList.add('p-channel_sidebar__name')
 
   category_icon_i = document.createElement('i')
   category_icon_i.classList.add('material-icons')
   category_icon_i.textContent = 'category'
 
   category_div = document.createElement('div')
-  // これつけたらいい感じの位置に表示してくれる
+  // これつけたら他と合わせていい感じの位置に表示してくれる
   category_div.classList.add('p-channel_sidebar__link')
+  category_div.classList.add('category')
   category_div.appendChild(category_icon_i)
   category_div.appendChild(category_name_span)
 
@@ -98,5 +127,67 @@ function add_category(channel_elements_div) {
   listitem_div.setAttribute('role', 'listitem')
   listitem_div.appendChild(category_div)
 
+  parent_channel = document.getElementsByClassName('categories')[0]
+
   channel_elements_div.insertBefore(listitem_div, parent_channel.nextSibling)
+}
+
+// category 配下に channel を追加
+function add_channel_to_category(channel_elements_div) {
+  let child_channel_name_regex = /lab3ec/
+  let category_name = 'lab3ec'
+
+  let child_channels = []
+  let category = null
+  Array.from(channel_elements_div.children).forEach(div => {
+    name_spans = div.getElementsByClassName('p-channel_sidebar__name')
+    if (name_spans.length == 1) {
+      name = name_spans[0].textContent
+      if (name == category_name) {
+        category = div
+      } else if (name.match(child_channel_name_regex)) {
+        child_channels.push(div)
+      }
+    }
+  })
+
+  category.nextSibling.classList.add('top_light_item')
+
+  let category_channels_div = document.createElement('div')
+  category_channels_div.classList.add('category_channels')
+  let sidebar = document.getElementsByClassName('p-channel_sidebar')[0]
+  category.appendChild(category_channels_div)
+
+  child_channels.forEach(category_channel => {
+    category_channels_div.appendChild(category_channel)
+//    category_channel.children[0].style.paddingLeft = '250px'
+    // onclick だと遅い、slack の処理との優先順位が前後する可能性があるため、押下の瞬間を取られる
+    category_channel.onmousedown = function(){
+      category_channel_clicked(category_channel)
+    }
+  })
+}
+
+// click 時に発火するイベント、css の再適用とか scroll 位置の調整とかを無理やり対処
+category_channel_clicked = function (category_channel) {
+  scrollable_classname = 'c-scrollbar__hider'
+  let scroll_area = document.getElementsByClassName(scrollable_classname)[0]
+  let scroll_offset = scroll_area.scrollTop
+
+  // このスーパー強引ハックよ、スクロール対象の class を削除して slack からの制御を無効化
+  // クラス外したら scrollTop が 0 になってしまうので、marginTop をいじって表示位置が動いていないように見せる
+  // ちょっと処理遅延するけど、まあ通常の仕様にとっては許容範囲でしょ
+  scroll_area.classList.remove(scrollable_classname)
+  scroll_area.style.marginTop = '-' + scroll_offset + 'px' 
+ 
+  let target = category_channel.getElementsByClassName('p-channel_sidebar__channel')[0]
+  let monitor = new MutationObserver(function(){
+    setTimeout(function(){
+      scroll_area.classList.add(scrollable_classname)
+      scroll_area.style.marginTop = '0px'
+      scroll_area.scrollTop = scroll_offset
+    }, 100)
+    monitor.disconnect()
+  }) 
+  monitor.observe(target, { attributes: true, attributeFilter: ['class']})
 }
